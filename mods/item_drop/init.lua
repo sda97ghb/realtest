10929c1b24d80ccf853acb36a6572998d132a0c3
Kotolegokot <gkotolegokot@gmail.com>
2012-11-28 22:12:50 +0600

    Disappearance of items creates a lot of lag. Removed it.

--- mods/item_drop/init.lua
+++ mods/item_drop/init.lua
@@ -27,9 +27,6 @@ function minetest.handle_node_drops(pos, drops, digger)
 					z = -z
 				end
 				obj:setvelocity({x=1/x, y=obj:getvelocity().y, z=1/z})
-				minetest.after(3600, function(obj)
-					obj:remove()
-				end, obj)
 			end
 		end
 	end
