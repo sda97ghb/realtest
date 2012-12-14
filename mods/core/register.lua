92388e8891145d5b621e2bafb27efa008f9d7f8c
Kotolegokot <gkotolegokot@gmail.com>
2012-12-14 08:51:44 +0600

    Added realtest.register_fuel and realtest.register_cooking

---
+++ mods/core/register.lua
@@ -0,0 +1,24 @@
+realtest.registered_fuels = {}
+realtest.registered_cooking = {}
+function realtest.register_fuel(FuelRef)
+	if FuelRef.input and FuelRef.output then
+		local fuel = {
+			input = FuelRef.input,
+			output = FuelRef.output,
+			time = FuelRef.time or 10,
+			max_temp = FuelRef.max_temp or 100,
+		}
+		realtest.registered_fuels[fuel.input] = fuel
+	end
+end
+function realtest.register_cooking(CookingRef)
+	if CookingRef.input and CookingRef.output then
+		local cooking = {
+			input = CookingRef.input,
+			output = CookingRef.output,
+			time = CookingRef.time or 10,
+			temperature = CookingRef.temperature or 100,
+		}
+		realtest.registered_cooking[cooking.input] = cooking 
+	end
+end
