22e06905e839e7c2aa04d7fa90c3272096ce4bce
Kotolegokot <gkotolegokot@gmail.com>
2012-12-17 15:49:20 +0600

    Fixed register_fuel

--- mods/core/register.lua
+++ mods/core/register.lua
@@ -7,9 +7,17 @@ function realtest.register_fuel(FuelRef)
 			total_heat = FuelRef.total_heat or 100,
 			burn_time = FuelRef.burn_time or 10,
 			max_temp = FuelRef.max_temp or 100,
-			used_in_furnace = FuelRef.used_in_furnace or true,
-			used_in_bonfire = FuelRef.used_in_bonfire or true,
 		}
+		if FuelRef.used_in_furnace == nil then
+			fuel.used_in_furnace = false
+		else
+			fuel.used_in_furnace = FuelRef.used_in_furnace
+		end
+		if FuelRef.used_in_bonfire == nil then
+			fuel.used_in_bonfire = false
+		else
+			fuel.used_in_bonfire = FuelRef.used_in_bonfire
+		end
 		realtest.registered_fuels[fuel.input] = fuel
 	end
 end
