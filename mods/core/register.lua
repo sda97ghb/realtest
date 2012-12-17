7bb7e9e9a7f13645cf8db836cf5e5ad75b8efb0d
Kotolegokot <gkotolegokot@gmail.com>
2012-12-17 13:45:09 +0600

    Fixed register functions

--- mods/core/register.lua
+++ mods/core/register.lua
@@ -4,8 +4,8 @@ function realtest.register_fuel(FuelRef)
 	if FuelRef.input and FuelRef.output then
 		local fuel = {
 			input = FuelRef.input,
-			output = FuelRef.output,
-			time = FuelRef.time or 10,
+			total_heat = FuelRef.total_heat or 100,
+			burn_time = FuelRef.burn_time or 10,
 			max_temp = FuelRef.max_temp or 100,
 			used_in_furnace = FuelRef.used_in_furnace or true,
 			used_in_bonfire = FuelRef.used_in_bonfire or true,
@@ -18,8 +18,8 @@ function realtest.register_cooking(CookingRef)
 		local cooking = {
 			input = CookingRef.input,
 			output = CookingRef.output,
-			time = CookingRef.time or 10,
-			temperature = CookingRef.temperature or 100,
+			specific_heat = CookingRef.specific_heat or 10,
+			melting_point = CookingRef.melting_point or 100,
 		}
 		realtest.registered_cooking[cooking.input] = cooking 
 	end
