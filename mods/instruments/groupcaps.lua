1e7f9978975c7d67be31aea067873d683a243ce6
Kotolegokot <gkotolegokot@gmail.com>
2012-12-19 22:48:03 +0600

    Fixed shovel groupcaps

--- mods/instruments/groupcaps.lua
+++ mods/instruments/groupcaps.lua
@@ -43,19 +43,19 @@ instruments.groupcaps = {
 		 fleshy={times={[2]=1.10, [3]=0.60},           uses=instruments.durability[13]*1.2, maxlevel=2}},
 	},
 	{--Shovels
-		{crumbly={times={[1]=9.40, [2]=4.05, [3]=4.70}, uses=instruments.durability[1], maxlevel=1}},--stone
-		{crumbly={times={[1]=5.40, [2]=4.05, [3]=2.70}, uses=instruments.durability[2], maxlevel=1}},--bismuth
-		{crumbly={times={[1]=5.00, [2]=3.75, [3]=2.50}, uses=instruments.durability[3], maxlevel=1}},--zinc
-		{crumbly={times={[1]=4.60, [2]=3.45, [3]=2.30}, uses=instruments.durability[4], maxlevel=1}},--tin
-		{crumbly={times={[1]=3.40, [2]=2.55, [3]=1.70}, uses=instruments.durability[5], maxlevel=2}},--copper
-		{crumbly={times={[1]=2.60, [2]=1.95, [3]=1.30}, uses=instruments.durability[6], maxlevel=2}},--rose gold
-		{crumbly={times={[1]=2.60, [2]=1.95, [3]=1.30}, uses=instruments.durability[7], maxlevel=2}},--black bronze
-		{crumbly={times={[1]=2.60, [2]=1.95, [3]=1.30}, uses=instruments.durability[8], maxlevel=2}},--bismuth bronze
-		{crumbly={times={[1]=2.20, [2]=1.75, [3]=1.10}, uses=instruments.durability[9], maxlevel=2}},--bronze
-		{crumbly={times={[1]=2.00, [2]=1.50, [3]=1.00}, uses=instruments.durability[10], maxlevel=2}},--aluminium
-		{crumbly={times={[1]=1.40, [2]=1.05, [3]=0.70}, uses=instruments.durability[11], maxlevel=3}},--wrought iron
-		{crumbly={times={[1]=1.00, [2]=0.75, [3]=0.50}, uses=instruments.durability[12], maxlevel=3}},--steel
-		{crumbly={times={[1]=0.40, [2]=0.30, [3]=0.20}, uses=instruments.durability[13], maxlevel=3}},--black steel
+		{crumbly={times={[1]=9.40, [2]=6.00, [3]=4.70}, uses=instruments.durability[1], maxlevel=1}},--stone
+		{crumbly={times={[1]=5.40, [2]=4.00, [3]=2.70}, uses=instruments.durability[2], maxlevel=1}},--bismuth
+		{crumbly={times={[1]=5.30, [2]=3.90, [3]=2.60}, uses=instruments.durability[3], maxlevel=1}},--zinc
+		{crumbly={times={[1]=5.20, [2]=3.80, [3]=2.50}, uses=instruments.durability[4], maxlevel=1}},--tin
+		{crumbly={times={[1]=5.10, [2]=3.70, [3]=2.40}, uses=instruments.durability[5], maxlevel=2}},--copper
+		{crumbly={times={[1]=5.00, [2]=3.60, [3]=2.30}, uses=instruments.durability[6], maxlevel=2}},--rose gold
+		{crumbly={times={[1]=5.00, [2]=3.60, [3]=2.30}, uses=instruments.durability[7], maxlevel=2}},--black bronze
+		{crumbly={times={[1]=5.00, [2]=3.60, [3]=2.30}, uses=instruments.durability[8], maxlevel=2}},--bismuth bronze
+		{crumbly={times={[1]=5.00, [2]=3.60, [3]=2.30}, uses=instruments.durability[9], maxlevel=2}},--bronze
+		{crumbly={times={[1]=5.00, [2]=3.60, [3]=2.30}, uses=instruments.durability[10], maxlevel=2}},--aluminium
+		{crumbly={times={[1]=4.90, [2]=3.50, [3]=2.20}, uses=instruments.durability[11], maxlevel=3}},--wrought iron
+		{crumbly={times={[1]=4.80, [2]=3.40, [3]=2.10}, uses=instruments.durability[12], maxlevel=3}},--steel
+		{crumbly={times={[1]=4.70, [2]=3.30, [3]=2.00}, uses=instruments.durability[13], maxlevel=3}},--black steel
 	},
 	{--Hammers
 		{cracky={times={[1]=14.0, [2]=10.5, [3]=7.00}, uses=instruments.durability[1], maxlevel=1}},--stone
