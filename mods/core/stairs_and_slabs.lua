3add4a8a1c9a2f6780ca41e1f9636b33b13103a7
Kotolegokot <gkotolegokot@gmail.com>
2013-03-23 16:32:56 +0600

    Slabs are wallmounted

--- mods/core/stairs_and_slabs.lua
+++ mods/core/stairs_and_slabs.lua
@@ -101,6 +101,7 @@ function realtest.register_slab(name, recipeitem, groups, images, description, s
 	if minetest.registered_nodes[name] then
 		recipeitem = recipeitem or name
 		groups = groups or minetest.registered_nodes[name].groups
+		groups.not_in_creative_inventory = 1
 		images = images or minetest.registered_nodes[name].tiles
 		description = description or minetest.registered_nodes[name].description .. " Slab"
 		sounds = sounds or minetest.registered_nodes[name].sounds
@@ -110,14 +111,32 @@ function realtest.register_slab(name, recipeitem, groups, images, description, s
 		end
 	end
 	drop = drop or name.."_slab"
-	minetest.register_node(":"..name.."_slab", {
-		description = description,
+	minetest.register_node(":"..name.."_slab_r", {
 		drawtype = "nodebox",
 		tiles = images,
 		drop = drop,
 		paramtype = "light",
+		paramtype2 = "wallmounted",
 		groups = groups,
 		sounds = sounds,
+		node_box = {
+			type = "wallmounted",
+			wall_bottom = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			wall_top = {-0.5, 0, -0.5, 0.5, 0.5, 0.5},
+			wall_side = {-0.5, -0.5, -0.5, 0, 0.5, 0.5},
+		},
+		selection_box = {
+			type = "wallmounted",
+			wall_bottom = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+			wall_top = {-0.5, 0, -0.5, 0.5, 0.5, 0.5},
+			wall_side = {-0.5, -0.5, -0.5, 0, 0.5, 0.5},
+		}
+	})
+	minetest.register_node(":"..name.."_slab", {
+		description = description,
+		drawtype = "nodebox",
+		tiles = images,
+		paramtype = "light",
 		node_box = {
 			type = "fixed",
 			fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
@@ -130,97 +149,23 @@ function realtest.register_slab(name, recipeitem, groups, images, description, s
 			if pointed_thing.type ~= "node" then
 				return itemstack
 			end
-
-			-- If it's being placed on an another similar one, replace it with
-			-- a full block
-			local slabpos = nil
-			local slabnode = nil
-			local p0 = pointed_thing.under
-			local p1 = pointed_thing.above
-			local n0 = minetest.env:get_node(p0)
-			if n0.name == name.."_slab" and
-					p0.y+1 == p1.y then
-				slabpos = p0
-				slabnode = n0
-			end
-			if slabpos then
-				-- Remove the slab at slabpos
-				minetest.env:remove_node(slabpos)
-				-- Make a fake stack of a single item and try to place it
-				local fakestack = ItemStack(recipeitem)
-				pointed_thing.above = slabpos
-				fakestack = minetest.item_place(fakestack, placer, pointed_thing)
-				-- If the item was taken from the fake stack, decrement original
-				if not fakestack or fakestack:is_empty() then
-					itemstack:take_item(1)
-				-- Else put old node back
-				else
-					minetest.env:set_node(slabpos, slabnode)
-				end
+			local p1 = pointed_thing.under
+			local p2 = pointed_thing.above
+			local dir = {x=p1.x-p2.x,y=p1.y-p2.y,z=p1.z-p2.z}
+			if minetest.env:get_node(pointed_thing.under).name == name.."_slab_r"
+				and minetest.env:get_node(pointed_thing.under).param2 == minetest.dir_to_wallmounted(dir) then
+				minetest.env:set_node(pointed_thing.under, {name=name})
+			elseif minetest.registered_nodes[minetest.env:get_node(pointed_thing.above).name].buildable_to then
+				minetest.env:set_node(pointed_thing.above, {name=name.."_slab_r", param2=minetest.dir_to_wallmounted(dir)})
+			else
 				return itemstack
 			end
 			
-			-- Upside down slabs
-			if p0.y-1 == p1.y then
-				-- Turn into full block if pointing at a existing slab
-				if n0.name == name.."_slab_upside_down" then
-					-- Remove the slab at the position of the slab
-					minetest.env:remove_node(p0)
-					-- Make a fake stack of a single item and try to place it
-					local fakestack = ItemStack(recipeitem)
-					pointed_thing.above = p0
-					fakestack = minetest.item_place(fakestack, placer, pointed_thing)
-					-- If the item was taken from the fake stack, decrement original
-					if not fakestack or fakestack:is_empty() then
-						itemstack:take_item(1)
-					-- Else put old node back
-					else
-						minetest.env:set_node(p0, n0)
-					end
-					return itemstack
-				end
-				
-				-- Place upside down slab
-				local fakestack = ItemStack(name.."_slab_upside_down")
-				local ret = minetest.item_place(fakestack, placer, pointed_thing)
-				if ret and ret:is_empty() then
-					itemstack:take_item()
-					return itemstack
-				end
+			if not minetest.setting_getbool("creative_mode") then
+				itemstack:take_item()
 			end
-			
-			-- If pointing at the side of a upside down slab
-			if n0.name == name.."_slab_upside_down" and
-					p0.y+1 ~= p1.y then
-				-- Place upside down slab
-				local fakestack = ItemStack(name.."_slab_upside_down")
-				local ret = minetest.item_place(fakestack, placer, pointed_thing)
-				if ret and ret:is_empty() then
-					itemstack:take_item()
-					return itemstack
-				end
-			end
-			
-			-- Otherwise place regularly
-			return minetest.item_place(itemstack, placer, pointed_thing)
+			return itemstack
 		end,
-	})
-	
-	minetest.register_node(":"..name.."_slab_upside_down", {
-		drop = drop,
-		drawtype = "nodebox",
-		tiles = images,
-		paramtype = "light",
-		groups = groups,
-		sounds = sounds,
-		node_box = {
-			type = "fixed",
-			fixed = {-0.5, 0, -0.5, 0.5, 0.5, 0.5},
-		},
-		selection_box = {
-			type = "fixed",
-			fixed = {-0.5, 0, -0.5, 0.5, 0.5, 0.5},
-		},
 	})
 
 	minetest.register_craft({
@@ -229,6 +174,7 @@ function realtest.register_slab(name, recipeitem, groups, images, description, s
 			{recipeitem, recipeitem, recipeitem},
 		},
 	})
+	minetest.register_alias(name.."_slab_upside_down", name.."_slab_r")
 end
 
 function realtest.register_stair_and_slab(name, recipeitem, groups, images, description, sounds, drop)
